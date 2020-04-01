dbefccb839b8adbe4676c40b88f8323bc083adb1
root <root@localhost.localdomain>
2020-03-31 23:13:26 -0400

    [New] Finalized

--- shell_example.c
+++ shell_example.c
@@ -15,11 +15,12 @@
 extern char **environ; /* defined by libc */
 /* function prototypes */
 void eval(char *cmdline);
-int phraseArgv(char *buf, char ***argvList);
+int phraseArgv(char **argvStringList, char ***argvList,int argvSize, char *input[], char *output[]);
+int phrasePipe(char *buf, char **argvStringList);
 void phraseIO(char *buf, char *cmdline, char *input, char *output);
 void removeSingleSymbol(char *line, int lineSize, int removePos);
 int builtin_command(char **argv);
-void runArgv(char ***argvs, int argvSize, char *input, char *output);
+void runArgv(char ***argvs, int argvSize, char *input[], char *output[]);
 void exeProc(char **argv,int fdIn, int fdOut,int pipesCount, int pfds[][2]);
 
 int main()
@@ -42,25 +43,27 @@ int main()
 void eval(char *cmdline)
 {
     char ***argv = (char***)malloc(MAXARGS*sizeof(char**));/* Argument list execve() */
+    char ** argvStringList =  (char**)malloc(MAXARGS*sizeof(char*));
     char buf[MAXLINE];   /* Holds modified command line */
-    char input[MAXLINE];   /* Holds input path*/
-    char output[MAXLINE];   /* Holds output path*/
+    char *input[MAXARGS];   /* Holds input path*/
+    char *output[MAXARGS];   /* Holds output path*/
     int bg;              /* Should the job run in bg or fg? */
     pid_t pid;           /* Process id */
 
-    phraseIO(buf, cmdline, input, output);
+    //phraseIO(buf, cmdline, input, output);
+    strcpy(buf,cmdline);
     
-    /*
-    printf("Input: %s\n", input);
-    printf("Output: %s\n", output);
-    printf("buf: %s\n",buf);
-    */
+    
     //strcpy(buf, cmdline);
-    int argvSize = phraseArgv(buf, argv);
+    int argvSize = phrasePipe(buf, argvStringList);
+    phraseArgv(argvStringList, argv, argvSize, input, output);
     /*
     int i=0;
     while(i<argvSize){
-        printf("argv%d: %s\n",i,argv[i][0]);
+        printf("argvString %d: %s\n",i,argvStringList[i]);
+        printf("argv %d: %s\n",i,argv[i][0]);
+        printf("input %d: %s\n",i,input[i]);
+        printf("output %d: %s\n",i,output[i]);
         i++;
     }
     */
@@ -87,27 +90,108 @@ void eval(char *cmdline)
     */
     return;
 }
-
+int phrasePipe(char *buf, char **argvStringList){
+    char *cmd = strtok(buf, "\n"); 
+    int argvSize = 0;
+    cmd = strtok(buf, "|"); 
+    while (cmd!=NULL){
+        argvStringList[argvSize] = (char*)malloc(MAXLINE*sizeof(char));
+        sprintf(argvStringList[argvSize], cmd);
+        cmd = strtok(NULL, "|"); 
+        argvSize++;
+    }
+    return argvSize;
+}
 void phraseIO(char *buf, char *cmdline, char *input, char *output){
-
+    char* inPtr,  *outPtr, *outputReader;
+    char *tmpBuf = (char*)malloc(MAXARGS*sizeof(char*));
+    int charTmp, isInitstart = 0;
+    strcpy(tmpBuf, cmdline);
     *input = '\0';
     *output = '\0';
-    cmdline = strtok(cmdline, "\n");
-    char* inPtr,  *outPtr, *inputReader, *outputReader;
-    //char inputReader[MAXLINE], outputReader[MAXLINE];
-    inPtr = strrchr(cmdline, '<');
+    //cmdline = strtok(cmdline, "\n");
+    /*
+    char *readPointer=NULL, *startPointer=NULL;
+    int state = 0; //0: cmd, 1: input, 2: output
+    for (readPointer = tmpBuf; *readPointer != '\0'; readPointer++) {
+        charTmp= (unsigned char) *readPointer;
+        if (!isInitstart) {
+            isInitstart = 1;
+            startPointer = readPointer;
+        }
+        switch (state) {
+            case 0:
+                if  (charTmp == '<' || charTmp == '>') {
+                    *readPointer = 0;
+                    printf("startPointer1: %s\n", startPointer);
+                    buf = startPointer;
+                    if (charTmp == '<'){
+                        state = 1;
+                    }
+                    if (charTmp == '>'){
+                        state = 2;
+                    }
+                    isInitstart = 0;
+                    printf("startPointer2: %s\n", startPointer);
+                }
+                continue;
+            case 1:
+                if  (charTmp == '>') {
+                    *readPointer = 0;
+                    printf("startPointer3: %s\n", startPointer);
+                    strcpy(input, startPointer);
+                    state = 2;
+                    isInitstart = 0;
+                    printf("startPointer4: %s\n", startPointer);
+                }
+                continue;
+
+        }
+    }
+    switch(state){
+        case 0:
+            printf("startPointer case0: %s\n", startPointer);
+            strcpy(buf, startPointer);
+            buf = strtok(buf, "\n");
+            break;
+        case 1:
+                    printf("startPointer case1: %s\n", startPointer);
+            strcpy(input, startPointer);
+            input = strtok(input, "\n");
+            break;
+        case 2:
+                    printf("startPointer case2: %s\n", startPointer);
+            strcpy(output, startPointer);
+            output = strtok(output, "\n");
+            break;
+
+    }
+    */
+    //printf("==phraseIO Start\n");
+    //printf("Input: %s\n",tmpBuf);
+    inPtr = strrchr(tmpBuf, '<');
+    outPtr = strrchr(tmpBuf, '>');
     if (inPtr){
-        inputReader = strtok(cmdline, "<"); 
-        strcpy(input,inputReader);
-        cmdline = strtok(NULL, "<"); 
+        tmpBuf = strtok(tmpBuf, "<"); 
+        strcpy(buf, tmpBuf);
+        //printf("inPtr buf: %s\n", buf);
+        tmpBuf = strtok(NULL, "<"); 
+        //printf("inPtr tmpBuf: %s\n", tmpBuf);
+        if (outPtr)
+            tmpBuf = strtok(tmpBuf, ">"); 
+        strcpy(input,tmpBuf);
     }
-    outPtr = strrchr(cmdline, '>');
     if (outPtr){
-        outputReader = strtok(cmdline, ">"); 
-        outputReader = strtok(NULL, ">"); 
-        strcpy(output,outputReader);
+        if (!inPtr)
+            outputReader = strtok(tmpBuf, ">"); 
+            outputReader = strtok(NULL, ">"); 
+            //printf("outputReader: %s\n", outputReader);
+            strcpy(output,outputReader);
     }
-    strcpy(buf,cmdline);
+    if (!inPtr){
+        strcpy(buf,tmpBuf);
+    }
+    //printf("==phraseIO End\n");
 }
 
 /* If first arg is a builtin command, run it and return true */
@@ -125,113 +209,49 @@ int builtin_command(char **argv)
 }
 
 /* Parse the command line and build the argv array */
-int phraseArgv(char *buf, char ***argv)
+int phraseArgv(char **argvStringList, char ***argvList, int argvSize, char *input[], char *output[])
 {
-    char *cmd = strtok(buf, "\n"); 
-    //int fileInput = -1;
-    printf("RunCmd: \n");
-    //printf("Input: %s\n", input);
-    //printf("Output: %s\n", output);
-    printf("buf: %s\n",buf);
-    /*
-    if (input[0]!='\0'){
-        printf("Opening Filr %s\n", input);
-        fileInput= open(input, O_RDWR);
-    }
-    
-    printf("fileInput: %d\n",fileInput);
-    */
-    int argvSize = 0;
-    cmd = strtok(buf, "|"); 
-    while (cmd!=NULL){
-        argv[argvSize] = (char**)malloc(MAXARGS*sizeof(char*));
-        //printf("cmd: %s\n", cmd);
+    //printf("==phraseArgv start\n");//DEBUG
+    //printf("argvSize: %d\n", argvSize);
+    char *cmd = NULL; 
+    int argvCounter = 0;
+    for (; argvCounter<argvSize; argvCounter++){
+        //printf("cmd: %s\n", argvStringList[argvCounter]);
         int state = 0; //0: Null, 1: in normal word, 2: in double-quote
         char *readPointer=NULL, *startPointer=NULL;
         int charTmp;
         int count = 0;
-        pid_t pid;           /* Process id */
-
-        //char currentCmd[MAXLINE];
-        //strcpy(currentCmd,cmd);
-        for (readPointer = cmd; count < MAXARGS && *readPointer != '\0'; readPointer++) {
-            //printf("readPointer: %s\n",readPointer);
-            charTmp= (unsigned char) *readPointer;
-            switch (state) {
-            case 0:
-                if (isspace(charTmp)) {
-                    continue;
-                }
-
-                if (charTmp== '"') {
-                    state = 2;
-                    startPointer = readPointer + 1; 
-                    continue;
-                }
-                else if (charTmp== '\'') {
-                    state = 3;
-                    startPointer = readPointer + 1; 
-                    continue;
-                }
-                state = 1;
-                startPointer = readPointer;
-                continue;
-
-            case 1:
-                //printf("chartmp: %c\n",charTmp);
-                if  (charTmp== '\"') {
-                    //printf("\" is found\n");
-                    removeSingleSymbol(startPointer,sizeof(startPointer),readPointer-startPointer);
-                }
-                if  (charTmp== '\'') {
-                    //printf("\' is found\n");
-                    removeSingleSymbol(startPointer,sizeof(startPointer),readPointer-startPointer);
-                }
-                if (isspace(charTmp)) {
-                    *readPointer = 0;
-                    argv[argvSize][count] = startPointer;
-                    count++;
-                    state = 0;
-                }
-                continue;
-            
-            
-            case 2:
-                if (charTmp== '"') {
-                    *readPointer = 0;
-                    argv[argvSize][count] = startPointer;
-                    count++;
-                    state = 0;
-                }
-                continue;
-            case 3:
-                if (charTmp== '\'') {
-                    *readPointer = 0;
-                    argv[argvSize][count] = startPointer;
-                    count++;
-                    state = 0;
-                }
-                continue;
-            }
-        }
-        if (state != 0 && count < MAXARGS){
-            //printf("startPointer: %s",startPointer);
-            argv[argvSize][count] = startPointer;
+        argvList[argvCounter] = (char**)malloc(MAXARGS*sizeof(char*));
+        char *cmdBuff;
+        cmdBuff = (char*)malloc(MAXLINE*sizeof(char));
+        input[argvCounter] = (char*)malloc(MAXLINE*sizeof(char));
+        output[argvCounter] = (char*)malloc(MAXLINE*sizeof(char));
+        phraseIO(cmdBuff, argvStringList[argvCounter], input[argvCounter], output[argvCounter]);
+        //printf("cmdBuff: %s\n", cmdBuff);
+        //printf("input[%d]: %s\n", argvCounter, input[argvCounter]);
+        //printf("output[%d]: %s\n", argvCounter, output[argvCounter]);
+        cmdBuff = strtok(cmdBuff, " \t");
+        
+        while (cmdBuff!=NULL && count<MAXARGS ){
+            //printf("cmdBuff in argv: %s\n", cmdBuff);
+            argvList[argvCounter][count] = (char*)malloc(MAXLINE*sizeof(char));
+            strcpy(argvList[argvCounter][count], cmdBuff);
+            //printf("argvList[%d][%d]: %s\n", argvCounter, count, argvList[argvCounter][count]);
             count++;
+            cmdBuff = strtok(NULL, " \t"); 
         }
-        cmd = strtok(NULL, "|"); 
-        argvSize++;
         
     }
+    
     //execvp(argv[0], argv);
     return argvSize;
 }
-void runArgv(char ***argvs, int argvSize, char *input, char *output)
+void runArgv(char ***argvs, int argvSize, char *input[], char *output[])
 {
     int procCounter, pipeCounter;
-    int outFd = STDOUT_FILENO;
     //int cmd_count = 0;
     //while (argvs[cmd_count]) { ++cmd_count; }
+    /*
     if (input[0]!='\0'){
         printf("Input: %s\n",input);
     }
@@ -242,7 +262,7 @@ void runArgv(char ***argvs, int argvSize, char *input, char *output)
         }
         outFd = open(output, O_RDWR|O_CREAT);
     }
-     
+     */
     int pfds[MAXARGS][2];
 
     for (pipeCounter = 0; pipeCounter < argvSize-1; ++pipeCounter)
@@ -257,8 +277,15 @@ void runArgv(char ***argvs, int argvSize, char *input, char *output)
     for (procCounter = 0; procCounter < argvSize; ++procCounter)
     {
         int fd_in = STDIN_FILENO;
-        //int fd_out = STDOUT_FILENO;
-        int fd_out = outFd;
+        if (input[procCounter][0]!='\0'){
+            input[procCounter] = strtok(input[procCounter], " \t");
+            fd_in = open(input[procCounter], O_RDONLY);
+        }
+        int fd_out = STDOUT_FILENO;
+        if (output[procCounter][0]!='\0'){
+            output[procCounter] = strtok(output[procCounter], " \t");
+            fd_out = open(output[procCounter], O_RDWR|O_CREAT);
+        }
         if (procCounter != 0) 
             fd_in = pfds[procCounter - 1][0];
         if (procCounter != argvSize - 1) 
@@ -278,6 +305,7 @@ void runArgv(char ***argvs, int argvSize, char *input, char *output)
         int status;
         wait(&status);
     }
+    
 }
 void exeProc(char **argv, int fdIn, int fdOut, int pipesCount, int pfds[][2])
 {
@@ -319,7 +347,7 @@ void removeSingleSymbol(char *line, int lineSize, int removePos){
         line[i] = line[i+1];
         i++;
     }
-    line[lineSize-1] = '\0';
+    //line[lineSize-1] = '\0';
     return;
 }
 
